a68872468b8c20e859f73400b85a959ce83c8f28
soburi59 <591959ant@gmail.com>
2023-04-11 15:06:17 +0900

    update 1-1-euler-2times.py

--- 2023/1-1-euler-2times.py
+++ 2023/1-1-euler-2times.py
@@ -1,8 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Apr 11 14:34:19 2023
-"""
-
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -17,6 +12,7 @@ h_list = [0.001, 0.02, 0.05]
 l = 0.5
 m = 1
 k = 1
+g = 9.80665
 
 # 初期条件
 theta0 = 1
@@ -27,25 +23,24 @@ for h in h_list:
     n = int((tf - t0) / h)
 
     # 時間と角度を格納する配列を作成
-    t = np.zeros(n+1)
+    t = np.linspace(t0, tf, n+1)
     theta = np.zeros(n+1)
-
-    # 初期条件を設定
-    t[0] = t0
+    theta_dot = np.zeros(n+1)
     theta[0] = theta0
+    theta_dot[0] = theta_dot0
 
     # オイラー法で微分方程式を解く
     for i in range(n):
-        t[i+1] = t[i] + h
-        theta[i+1] = theta[i] + h*theta_dot0
-        theta_dot0 -= h*(k/m)*np.sin(theta[i])
+        theta_ddot = -(k/m)*theta_dot[i] - (g/l)*np.sin(theta[i])
+        theta_dot[i+1] = theta_dot[i] + h*theta_ddot
+        theta[i+1] = theta[i] + h*theta_dot[i]
 
     # グラフを描画
     plt.plot(t, theta, label=f"h={h}")
 
 # グラフの設定
-plt.xlabel("Time")
-plt.ylabel("Angle")
-plt.title("Simulation of Simple Pendulum")
+plt.xlabel("Time[sec]")
+plt.ylabel("θ[rad]")
+plt.title("Simulation of a pendulum using the Euler method")
 plt.legend()
 plt.show()
