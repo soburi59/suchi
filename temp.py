0d778110e1be9d55681e9458035dd7738ccf34f6
soburi <591959ant@gmail.com>
2023-06-01 07:36:28 +0900

    update temp.py

--- temp.py
+++ temp.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri May 26 11:44:04 2023
+アフィン変換
 
 """
 
 import numpy as np
 
-def rotate_point(x, y, x0, y0, theta):
+def rotate_point(x, y, x0, y0, theta):#x,yの点をx0,y0を中心にtheta反時計回りに回転
+    print("回転前の座標: ({}, {}, {})".format(x, y, theta))
     # 行列計算用の行列を定義
     matrix1 = np.array([[1, 0, 0], [0, 1, 0], [-x0, -y0, 1]])
     matrix2 = np.array([[np.cos(theta), np.sin(theta), 0], [-np.sin(theta), np.cos(theta), 0], [0, 0, 1]])
@@ -17,7 +19,6 @@ def rotate_point(x, y, x0, y0, theta):
 
     return transformed_point[0], transformed_point[1] ,transformed_point[2]
 
-# 点(100, 20)を点(50, 50)を中心に70度左回りに回転させる
-x, y, z = rotate_point(100, 20, 50, 50, np.radians(-70))
-
+x, y, z = rotate_point(100, 20, 50, 50, np.deg2rad(70))
+#x, y, z = rotate_point(0,50, 0, 0, np.deg2rad(-90))
 print("回転後の座標: ({}, {}, {})".format(x, y, z))
